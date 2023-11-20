bbd717f9f9d9b07fc4240f7477b5b1f4331bb7fe
rodion02 <r.shkokov@g.nsu.ru>
2023-11-20 19:17:49 +0700

    bug_fix

--- release/check.py
+++ release/check.py
@@ -1,16 +1,18 @@
 from parser_part import *
 from tree import *
 from feedback import *
+import os
 
 def check_file(path): 
+    name = os.path.basename(path)
     txt = parse(path)
 
     tree = Make_tree()
     dcts = tree.walk(txt)
     # tree.show()
 
-    feedback = fb(dcts, path)
-    print(feedback)
+    new_path = path.replace(name, "fixed_{}".format(name))
+    feedback = fb(dcts, new_path)
     feedback = list(map(lambda x: 'MISSING: ' + ', '.join(map(str, x[:3])), feedback))
     feedback = '\n'.join(feedback)
     f = open("feedback.txt", "w", encoding="utf-8")
