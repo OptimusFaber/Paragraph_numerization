c1d7f2d3ec6037c15346c983b85413f0e01f8758
rodion02 <r.shkokov@g.nsu.ru>
2023-12-08 12:39:00 +0700

    add json

--- release/check.py
+++ release/check.py
@@ -2,6 +2,7 @@ from parser_part import *
 from tree import *
 from feedback import *
 import os
+import json
 
 def check_file(path, test=False, visualize=False): 
     name = os.path.basename(path)
@@ -16,7 +17,20 @@ def check_file(path, test=False, visualize=False):
     else:
         new_path = path.replace(name, "fixed_{}".format(name))
         feedback = fb(dcts, path, new_path)
+        # dictionary = dict(zip([i for i in range(len(feedback))], feedback))
+        dictionary = []
+        for i in range(len(feedback)):
+            dictionary.append({"TypeError": feedback[i][0],
+                       "ErrorLine": feedback[i][1],
+                       "LineNumber": feedback[i][2],
+                       "Description": feedback[i][3],
+                       "PrevLine": feedback[i][4],
+                       "NextLine": feedback[i][5]})
         feedback = list(map(lambda x: "{}: {} || {} || {} || {} || {}".format(*x), feedback))
+
+        json_object = json.dumps(dictionary, indent=4)
+        with open("feedback.json", "w") as outfile:
+            outfile.write(json_object)
         feedback = '\n'.join(feedback)
         f = open("feedback.txt", "w", encoding="utf-8")
         f.write(feedback)
