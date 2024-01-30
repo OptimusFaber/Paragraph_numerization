ec49bd13493cd2cb278700a5856000a74d9486fe
rodion02 <r.shkokov@g.nsu.ru>
2024-01-30 18:43:13 +0700

    bug fix

--- old_versions/v8/release/main.py
+++ old_versions/v8/release/main.py
@@ -1,5 +1,5 @@
 from check import check_file
 
-file_txt = "text_docx_libreoffice.txt"
+file_txt = "error.txt"
 config_json = "472.json"
 check_file(txt_path=file_txt, json_path=config_json, visualize=True)
--- old_versions/v8/release/tree.py
+++ old_versions/v8/release/tree.py
@@ -104,13 +104,21 @@ class Make_tree:
             if dif > 0:
                 for _ in range(dif):
                     elem1.append(1)
-            elif dif < 0:
-                for _ in range(abs(dif)):
-                    elem2.append(1)
+            # elif dif < 0:
+            #     for _ in range(abs(dif)):
+            #         elem2.append(1)
+            flag = True
             for i in range(1, min(len(elem1), len(elem2))):     ## Вдруг после 5.4 идет 5.6 + ищем разницу между параграфами
-                cnt += abs(elem1[i] - elem2[i])
-                if elem1[i] > elem2[i]:
-                    return False
+                if flag:
+                    if elem1[i] > elem2[i]:
+                        return False
+                    elif elem1[i] < elem2[i]:
+                        cnt += abs(elem1[i] - elem2[i])
+                        flag = False
+                    else:
+                        continue
+                else:
+                    cnt += (elem2[i]-1)
             if cnt > 3:
                 return False
             return True
@@ -159,15 +167,23 @@ class Make_tree:
                         self.tree.append(Node(elem[1] + " " + self.revfunc(i), sign=elem[1], pos=elem[2], parent=parent, data_type=elem[3], status='MISSING', delimetr = elem[4]))
                         self.non_txt_dct[elem[1]].append(self.revfunc(i))
                     except:
-                        self.tree.append(Node(" " + elem[1] + " " + self.revfunc(i), sign=elem[1], pos=elem[2], parent=parent, data_type='None', status='DUPLICATE', delimetr = None))                        
+                        try:
+                            self.tree.append(Node(" " + elem[1] + " " + self.revfunc(i), sign=elem[1], pos=elem[2], parent=parent, data_type='None', status='DUPLICATE', delimetr = None))
+                        except:
+                            return                        
             try:
                 self.tree.append(Node(elem[1] + " " + elem[0], sign=elem[1], pos=elem[2], parent=parent, data_type=elem[3], status='EXISTING', delimetr = elem[4]))
                 self.non_txt_dct[elem[1]].append(elem[0])
             except:
-                self.tree.append(Node(" " + elem[1] + " " + elem[0], sign=elem[1], pos=elem[2], parent=parent, data_type='None', status='DUPLICATE', delimetr = None))
+                try:
+                    self.tree.append(Node(" " + elem[1] + " " + elem[0], sign=elem[1], pos=elem[2], parent=parent, data_type='None', status='DUPLICATE', delimetr = None))
+                except:
+                    return
         
     def letters_romans(self, elem, k):      ## Алгоритм работы с буквенными параграфами
         parent = None
+        if elem[2] == 940:
+            print()
         if self.func(elem[0]) == self.func(self.n) and self.tree[-1].data_type != elem[3]:   ## Вдруг это первый элемент последовательности
             for i in range(-1, max(-len(self.tree)-1, -NUM_PARAGRAPH_SEARCH), -1):
                 parent=self.tree[-1]
@@ -176,6 +192,9 @@ class Make_tree:
                         self.tree.append(Node(" " + elem[0], sign=elem[1], pos=elem[2], parent=parent, data_type='None', status='DUPLICATE', delimetr = None))
                         return False
                     break
+            self.tree.append(Node(elem[0], sign=elem[1], pos=elem[2], parent=parent, data_type=elem[3], status='EXISTING', delimetr = elem[4]))
+        elif self.func(elem[0]) == self.func(self.n):
+            parent=self.tree[-1]
             self.tree.append(Node(elem[0], sign=elem[1], pos=elem[2], parent=parent, data_type=elem[3], status='EXISTING', delimetr = elem[4]))
         else:
             for i in range(-1, max(-len(self.tree)-1, LETTER_SEARCH), -1):  
@@ -211,6 +230,10 @@ class Make_tree:
             st = 0
             if self.ancestor and self.tree:    ## если это не новое дерево 
                 for i in range(k+1, min(len(self.lst), k+NUMBER_SEARCH)):   ## иду по нераспределенным элементам, смотрю что впереди
+                    if self.tree[-1].sign == 'таблица':
+                        parent = self.tree[-1]
+                        st = True
+                        break
                     param = ok = False
                     if 'number' not in self.lst[i][3]: continue
                     if self.similarity_check(elem, self.lst[i]):
@@ -256,8 +279,12 @@ class Make_tree:
                     self.p.append(elem[1])
         else:
             black_list = set()
+            point = list(self.tree[-1].ancestors)
+            parent_flag = False
             for i in range(-1, max(-len(self.tree)-1, -NUMBER_SEARCH), -1):  
                 if self.similarity_check(self.tree[i], elem):
+                    if len(point) < len(list(self.tree[i].ancestors)): continue
+                    if parent_flag and self.tree[i].parent.name != 'txt': continue
                     if self.tree[i].parent in black_list:
                         continue
                     if self.logic_check(self.tree[i], elem):
@@ -314,11 +341,13 @@ class Make_tree:
         parent = None
         sp = []
         black_list = set()
-        for i in range(-1, max(-len(self.tree)-1, -NUM_PARAGRAPH_SEARCH), -1):
+        forbiden_list = list()
+        for i in range(-1, -len(self.tree)-1, -1):
             if 'number' in self.tree[i].data_type and (elem[1] == self.tree[i].sign or self.tree[i].sign == 'NaN'):
-                if self.numeral_check(self.tree[i], elem) and (self.tree[i].parent not in black_list):
+                if self.tree[i].name == elem[0]: forbiden_list.append(self.tree[i].parent)
+                if self.numeral_check(self.tree[i], elem) and all([ancestor not in black_list for ancestor in self.tree[i].ancestors]) and (self.tree[i] not in forbiden_list):
                     node = self.tree[i]
-                    if elem[2] - node.pos > 326: break
+                    # if elem[2] - node.pos > 326: break
                     ## добавить определение parent-a тк он думает что отец 1.3.1 это 1.2.1
                     parent, delimetr = node, node.delimetr
                     rel = list(map(int, node.node_name.split('.')))
@@ -342,7 +371,10 @@ class Make_tree:
                             k1 = k2 = False
                             if e==0:
                                 for j in range(rel[e]+1, sp[e]):
-                                    self.tree.append(Node('{}.1'.format(j), sign='.', pos=elem[2], parent=parent, data_type='numbers', status='MISSING', delimetr = delimetr))  
+                                    try:
+                                        self.tree.append(Node('{}.1'.format(j), sign='.', pos=elem[2], parent=parent, data_type='numbers', status='MISSING', delimetr = delimetr))  
+                                    except:
+                                        continue
                                     param = True
                             else:
                                 for j in range(rel[e]+1, sp[e]):  
@@ -368,6 +400,8 @@ class Make_tree:
                                     parent = parent.parent
                     if len(node.node_name) == 1:
                         parent = parent.parent
+                    # if len(node.name.split('.')) == len(elem[0].split('.')):
+                    #     parent = parent.parent
                     self.tree.append(Node(elem[0], sign=elem[1], pos=elem[2], parent=parent, data_type=elem[3], status='EXISTING', delimetr = elem[4]))
                     self.ancestor = self.tree[-1]  
                     return
@@ -432,12 +466,16 @@ class Make_tree:
                             self.tree.append(Node(" " + "{}.{}".format(sp[0], i), sign=elem[1], pos=elem[2], parent=parent, data_type='None', status='DUPLICATE', delimetr = None))
                     self.tree.append(Node(elem[0], sign='.', pos=elem[2], parent=parent, data_type='numbers', status='EXISTING', delimetr = elem[4]))
                     self.ancestor = self.tree[-1]
-                
+            else:
+                if forbiden_list:
+                    self.tree.append(Node(" {}".format(elem[0]), sign=elem[1], pos=elem[2], parent=forbiden_list[0], data_type='None', status='DUPLICATE', delimetr = None))
 
     def walk(self, lst):
         self.lst = lst
         for elem, k in zip(self.lst, range(len(self.lst))):
             self.k = k
+            if elem[0] == "4.1.1.1.1":
+                print()
             if elem[1] in ["таблица", "рисунок", "рис", "схема"]:
                 self.func, self.revfunc = functions[elem[3]]
                 self.n = first_elements[elem[3]]
