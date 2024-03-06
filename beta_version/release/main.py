3d14b23c41c3e57ef6d6e53901069865d9cf9963
rodion02 <r.shkokov@g.nsu.ru>
2024-03-06 11:17:56 +0700

    bug fix

--- beta_version/release/main.py
+++ beta_version/release/main.py
@@ -1,5 +1,5 @@
 from check import check_file
 
-file_txt = "mistake4.txt"
+file_txt = "mistake3.txt"
 config_json = "config.json"
 check_file(txt_path=file_txt, json_path=config_json, visualize=True)
